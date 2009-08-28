cd9f44b87019d59a52e4e52efec3c8a7728465f2
Anthony Pitluga <tony@pitluga-macbook-2.local>
2009-08-28 19:45:15 -0400

    refactored @instances to @running_instances

--- lib/awsymandias/ec2/application_stack.rb
+++ lib/awsymandias/ec2/application_stack.rb
@@ -25,7 +25,7 @@ module Awsymandias
         @name       = name
         @roles      = opts[:roles] || {}
         @sdb_domain = opts[:sdb_domain] || DEFAULT_SDB_DOMAIN
-        @instances  = {}
+        @running_instances  = {}
         yield self if block_given?
       end
 
@@ -33,13 +33,13 @@ module Awsymandias
         opts = names.extract_options!
         names.each do |name|
           @roles[name] = opts
-          self.metaclass.send(:define_method, name) { @instances[name] }
+          self.metaclass.send(:define_method, name) { @running_instances[name] }
         end
       end
 
       def launch
         @roles.each do |name, params| # TODO Optimize this for a single remote call.
-          @instances[name] = Awsymandias::EC2::Instance.launch(params)
+          @running_instances[name] = Awsymandias::EC2::Instance.launch(params)
         end
         store_role_to_instance_id_mapping!
         self
@@ -47,31 +47,31 @@ module Awsymandias
 
       def reload
         raise "Can't reload unless launched" unless launched?
-        @instances.values.each(&:reload) # TODO Optimize this for a single remote call.
+        @running_instances.values.each(&:reload) # TODO Optimize this for a single remote call.
         self
       end
 
       def terminate!
-        @instances.values.each(&:terminate!) # TODO Optimize this for a single remote call.
+        @running_instances.values.each(&:terminate!) # TODO Optimize this for a single remote call.
         remove_role_to_instance_id_mapping!
         self
       end
 
       def launched?
-        @instances.any? || ( @instances = retrieve_role_to_instance_id_mapping ).any?
+        @running_instances.any? || ( @running_instances = retrieve_role_to_instance_id_mapping ).any?
       end
 
       def running?
-        launched? && @instances.values.all?(&:running?)
+        launched? && @running_instances.values.all?(&:running?)
       end
 
       def port_open?(port)
-        @instances.values.all? { |instance| instance.port_open?(port) }
+        @running_instances.values.all? { |instance| instance.port_open?(port) }
       end
 
       def running_cost
         return Money.new(0) unless launched?
-        @instances.values.sum { |instance| instance.running_cost }
+        @running_instances.values.sum { |instance| instance.running_cost }
       end
 
       def inspect
@@ -84,7 +84,7 @@ module Awsymandias
 
       def store_role_to_instance_id_mapping!
         Awsymandias::SimpleDB.put @sdb_domain, @name, ( returning({}) do |h|
-          @instances.each {|role_name, instance| h[role_name] = instance.instance_id}
+          @running_instances.each {|role_name, instance| h[role_name] = instance.instance_id}
         end )
       end
 
